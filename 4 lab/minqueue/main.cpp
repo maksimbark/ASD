901834fbd01ebe336eed337f15b75829934c9a80
maksimbarkalov <maks@fizmatspb.ru>
2017-03-24 03:20:25 +0300

    Minimal queue

--- 4 lab/minqueue/main.cpp
+++ 4 lab/minqueue/main.cpp
@@ -1,52 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <string>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
+    int miniter = 0;
     ifstream cin("queuemin.in");
     ofstream cout("queuemin.out");
-    long long n, i, cu, min, answer;
-    vector< vector<int> > a, b;
-    min = 1000000001;
-    vector<int> pushy;
+    long long n, i, cu;
+    vector<long long> pushy;
     cin >> n;
     string curr;
     for (i = 0; i < n; i++) {
         cin >> curr;
         if (curr == "-") {
-            if (b.size() == 0) {
-                reverse(a.begin(), a.end());
-                b = a;
-                a.clear();
-            }
-            b.pop_back();
-
+            //pushy.erase(pushy.begin());
+            miniter++;
         } else if (curr == "+") {
             cin >> cu;
-            if (min > cu) {
-                min = cu;
-            }
             pushy.push_back(cu);
-            pushy.push_back(min);
-            a.push_back(pushy);
-            pushy.clear();
+            if (pushy.size() - miniter > 1) {
+                for (int i = miniter; i < pushy.size(); i++) {
+                    if (pushy[i] > cu) {
+                        pushy[i] = cu;
+                    }
+                }
+            }
         } else {
-            answer = 1000000001;
-            for (int i = 0; i < b.size(); i++) {
-                if (b[i][0] < answer) {
-                    answer = b[i][0];
-                }
-            }
-            for (int i = 0; i < a.size(); i++) {
-                if (a[i][1] < answer) {
-                    answer = a[i][1];
-                }
-            }
-            cout << answer << endl;
+cout << pushy[miniter] << endl;
         }
     }
 
