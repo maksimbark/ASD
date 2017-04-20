eba7b0a049dc62e442f8389010009dfe92d2621e
maksimbarkalov <maks@fizmatspb.ru>
2017-04-21 00:56:26 +0300

    Komponents (task 5) added

--- 6 lab/shortestWay/main.cpp
+++ 6 lab/shortestWay/main.cpp
@@ -8,27 +8,29 @@ struct listik {
 };
 
 struct node {
-    listik* cat;
+    listik *cat;
     string color;
 };
 
-int qcount = 0;
-vector<int> answer(100001);
-vector<node *> graph(100001);
+int qcount = 1;
+vector<int> answer(30001);
+vector<node *> graph(30001);
 queue<int> bfs;
 
 void search(int curr) {
     graph[curr]->color = "grey";
-    listik* temp = graph[curr]->cat;
+    listik *temp = graph[curr]->cat;
     while (temp) {
         if (graph[temp->kuda]->color == "white") {
-            //graph[temp->kuda]->color = "grey";
+            if (!answer[temp->kuda]) {
+                answer[temp->kuda] = qcount;
+            }
             bfs.push(temp->kuda);
             temp = temp->next;
         } else break;
     }
     graph[curr]->color = "black";
-    answer[curr] = qcount;
+    qcount++;
     bfs.pop();
     if (bfs.size() > 0) {
         search(bfs.front());
@@ -36,13 +38,13 @@ void search(int curr) {
 }
 
 int main() {
-    ifstream fin("components.in");
-    ofstream fout("components.out");
+    ifstream fin("pathbge1.in");
+    ofstream fout("pathbge1.out");
     int size, count;
     int from, where;
     fin >> size >> count;
     for (int i = 1; i <= size; i++) {
-        node* temp = new(node);
+        node *temp = new(node);
         temp->color = "white";
         temp->cat = NULL;
         graph[i] = temp;
@@ -53,14 +55,14 @@ int main() {
         if (from > where) {
             swap (from, where);
         }
-        if(!graph[from]->cat) {
+        if (!graph[from]->cat) {
             listik *add = new(listik);
             add->kuda = where;
             add->next = NULL;
             graph[from]->cat = add;
         } else {
             listik *temp = graph[from]->cat;
-            while(temp->next) {
+            while (temp->next) {
                 temp = temp->next;
             }
             listik *add = new(listik);
@@ -70,16 +72,11 @@ int main() {
         }
     }
 
-    for (int i = 1; i <= size; i++) {
-        if (graph[i]->color == "white") {
-            qcount++;
-            bfs.push(i);
-            search(i);
-        }
-    }
-    fout << qcount << endl;
-    for (auto q: answer) {
-        if (q) fout << q << " ";
+    fout << 0 << " ";
+    bfs.push(1);
+    search(1);
+    for (int i = 2; i <= size; i++) {
+        fout << answer[i] << " ";
     }
     return 0;
 }
