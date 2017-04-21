bddaf090fda07f8ad381c5b9c6b60b2ca269ea17
maksimbarkalov <maks@fizmatspb.ru>
2017-04-21 20:35:12 +0300

    normalSearch

--- 6 lab/labirintik/main.cpp
+++ 6 lab/labirintik/main.cpp
@@ -9,34 +9,49 @@ struct point {
 int n, m;
 vector<vector<char>> symbols(100, vector<char>(100));
 vector<vector<bool>> checked(100, vector<bool>(100));
-queue<pair<int, int>> bfs;
-int minStep = 1000000000;
-bool found = false;
-vector<char> answer;
 
-void search(int x, int y, int currStep, int from, vector<char> currAnsw) {
 
-    if (symbols[x][y] == 'T' && minStep > currStep) {
-        minStep = currStep;
-        answer = currAnsw;
-        found = true;
+void search(pair<int, int> curr, int now) {
+    queue<pair<int, int>> bfs;
+    bfs.push(curr);
+    checked[bfs.front().first][bfs.front().second] = true;
+    if (symbols[bfs.front().first][bfs.front().second] == 'T') {
+        cout << "T was found" << endl << bfs.front().first << " " << bfs.front().second << endl << "Path took " << now << " meters " << endl;
+        checked[bfs.front().first][bfs.front().second] = false;
     } else {
-        if (x - 1 >= 0 && from != 3 && symbols[x - 1][y] != '#') {
-            currAnsw.push_back('U');
-            search(x - 1, y, currStep + 1, 2, currAnsw);
+        if (bfs.front().first - 1 >= 0 && symbols[bfs.front().first - 1][bfs.front().second] != '#' &&
+            !checked[bfs.front().first - 1][bfs.front().second]) {
+            pair<int, int> temp;
+            temp.first = bfs.front().first - 1;
+            temp.second = bfs.front().second;
+            bfs.push(temp);
         }
-        if (x + 1 < n && from != 2 && symbols[x + 1][y] != '#') {
-            currAnsw.push_back('D');
-            search(x + 1, y, currStep + 1, 3, currAnsw);
+        if (bfs.front().first + 1 < n && symbols[bfs.front().first + 1][bfs.front().second] != '#' &&
+            !checked[bfs.front().first + 1][bfs.front().second]) {
+            pair<int, int> temp;
+            temp.first = bfs.front().first + 1;
+            temp.second = bfs.front().second;
+            bfs.push(temp);
         }
-        if (y - 1 >= 0 && from != 0 && symbols[x][y - 1] != '#') {
-            currAnsw.push_back('L');
-            search(x, y - 1, currStep + 1, 1, currAnsw);
+        if (bfs.front().second - 1 >= 0 && symbols[bfs.front().first][bfs.front().second - 1] != '#' &&
+            !checked[bfs.front().first][bfs.front().second - 1]) {
+            pair<int, int> temp;
+            temp.first = bfs.front().first;
+            temp.second = bfs.front().second - 1;
+            bfs.push(temp);
         }
-        if (y + 1 < m && from != 1 && symbols[x][y + 1] != '#') {
-            currAnsw.push_back('R');
-            search(x, y + 1, currStep + 1, 0, currAnsw);
+        if (bfs.front().second + 1 < m && symbols[bfs.front().first][bfs.front().second + 1] != '#' &&
+            !checked[bfs.front().first][bfs.front().second + 1]) {
+            pair<int, int> temp;
+            temp.first = bfs.front().first;
+            temp.second = bfs.front().second + 1;
+            bfs.push(temp);
         }
+    }
+    bfs.pop();
+    while (bfs.size() > 0) {
+        search(bfs.front(), now+1);
+        bfs.pop();
     }
 }
 
@@ -58,14 +73,11 @@ int main() {
         }
     }
 
-    search(S.x, S.y, 0, -1, answer);
-    if (found) {
-        fout << minStep << endl;
-        for (auto q: answer) {
-            fout << q;
-        }
-    } else {
-        fout << -1;
-    }
+    pair<int, int> temp;
+    temp.first = S.x;
+    temp.second = S.y;
+   // bfs.push(temp);
+    search(temp, 0);
+
     return 0;
 }
