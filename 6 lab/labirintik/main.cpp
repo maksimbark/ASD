ad0e2fd6b85ea4997fffc8028e885aea1f43ad67
maksimbarkalov <maks@fizmatspb.ru>
2017-04-21 20:52:15 +0300

    BFS fixed
    todo: add path

--- 6 lab/labirintik/main.cpp
+++ 6 lab/labirintik/main.cpp
@@ -10,14 +10,13 @@ int n, m;
 vector<vector<char>> symbols(100, vector<char>(100));
 vector<vector<bool>> checked(100, vector<bool>(100));
 
-
-void search(pair<int, int> curr, int now) {
-    queue<pair<int, int>> bfs;
-    bfs.push(curr);
+queue<pair<int, int>> bfs;
+queue<int> paths;
+void search(pair<int, int> curr) {
     checked[bfs.front().first][bfs.front().second] = true;
     if (symbols[bfs.front().first][bfs.front().second] == 'T') {
-        cout << "T was found" << endl << bfs.front().first << " " << bfs.front().second << endl << "Path took " << now << " meters " << endl;
-        checked[bfs.front().first][bfs.front().second] = false;
+        cout << "T was found" << endl << bfs.front().first << " " << bfs.front().second << endl << "Path took " << paths.front()
+             << " meters " << endl;
     } else {
         if (bfs.front().first - 1 >= 0 && symbols[bfs.front().first - 1][bfs.front().second] != '#' &&
             !checked[bfs.front().first - 1][bfs.front().second]) {
@@ -25,6 +24,7 @@ void search(pair<int, int> curr, int now) {
             temp.first = bfs.front().first - 1;
             temp.second = bfs.front().second;
             bfs.push(temp);
+            paths.push(paths.front()+1);
         }
         if (bfs.front().first + 1 < n && symbols[bfs.front().first + 1][bfs.front().second] != '#' &&
             !checked[bfs.front().first + 1][bfs.front().second]) {
@@ -32,6 +32,7 @@ void search(pair<int, int> curr, int now) {
             temp.first = bfs.front().first + 1;
             temp.second = bfs.front().second;
             bfs.push(temp);
+            paths.push(paths.front()+1);
         }
         if (bfs.front().second - 1 >= 0 && symbols[bfs.front().first][bfs.front().second - 1] != '#' &&
             !checked[bfs.front().first][bfs.front().second - 1]) {
@@ -39,6 +40,7 @@ void search(pair<int, int> curr, int now) {
             temp.first = bfs.front().first;
             temp.second = bfs.front().second - 1;
             bfs.push(temp);
+            paths.push(paths.front()+1);
         }
         if (bfs.front().second + 1 < m && symbols[bfs.front().first][bfs.front().second + 1] != '#' &&
             !checked[bfs.front().first][bfs.front().second + 1]) {
@@ -46,12 +48,13 @@ void search(pair<int, int> curr, int now) {
             temp.first = bfs.front().first;
             temp.second = bfs.front().second + 1;
             bfs.push(temp);
+            paths.push(paths.front()+1);
         }
     }
     bfs.pop();
-    while (bfs.size() > 0) {
-        search(bfs.front(), now+1);
-        bfs.pop();
+    paths.pop();
+    if (bfs.size() > 0) {
+        search(bfs.front());
     }
 }
 
@@ -76,8 +79,9 @@ int main() {
     pair<int, int> temp;
     temp.first = S.x;
     temp.second = S.y;
-   // bfs.push(temp);
-    search(temp, 0);
+    paths.push(0);
+    bfs.push(temp);
+    search(temp);
 
     return 0;
 }
