7151339ea0ae57c34a1d6f32f867bf312fec8b6d
maksimbarkalov <maks@fizmatspb.ru>
2017-04-26 21:22:50 +0300

    ALL 6 LAB ACCEPTED

--- 6 lab/cycleSearch/main.cpp
+++ 6 lab/cycleSearch/main.cpp
@@ -1,73 +1,50 @@
 #include <bits/stdc++.h>
 //be careful, it's WA version!
 using namespace std;
-struct graph {
-    int kuda;
-    graph *next;
-};
-vector<graph *> myGraph(100001);
-vector<char> colors(100001);
-vector<int> answer;
-bool ruse = false;
-int crutch = -1;
 
-void search(int gde) {
-    if (colors[gde] == 'w') {
-        colors[gde] = 'g';
-        graph *temp = myGraph[gde];
-        while (temp) {
-            if (colors[temp->kuda] == 'w') search(temp->kuda); else if (colors[temp->kuda] == 'g') {
-                ruse = true;
-                crutch = temp->kuda;
-                break;
+vector<vector<int>> myGraph(100001);
+vector<char> status(100001, 'w');
+vector<int> dfs;
+
+void search(int place) {
+    status[place] = 'q';
+    dfs.push_back(place);
+    for (auto q: myGraph[place]) {
+        if (status[q] == 'w') {
+            search(q);
+        } else if (status[q] == 'q') {
+            cout << "YES" << endl;
+            bool find = false;
+            for (auto w: dfs) {
+                if (w == q)
+                    find = true;
+                if (find)
+                    cout << w << " ";
             }
-            temp = temp->next;
+
+            exit(0);
         }
-        if (ruse) answer.push_back(gde);
-        if (gde == crutch) ruse = false;
-        colors[gde] = 'b';
+
     }
+    status[place] = 'b';
+    dfs.pop_back();
 }
 
 int main() {
     ifstream fin("cycle.in");
-    ofstream fout("cycle.out");
-    int n, m, a, b;
+    freopen("cycle.out", "w", stdout);
+    int n, m, from, where;
     fin >> n >> m;
 
     for (int i = 0; i < m; i++) {
-        fin >> a >> b;
-        if (!myGraph[a]) {
-            graph *temp = new(graph);
-            temp->kuda = b;
-            colors[a] = 'w';
-            colors[b] = 'w';
-            temp->next = NULL;
-            myGraph[a] = temp;
-        } else {
-            graph *temp = myGraph[a];
-            while (temp->next) {
-                temp = temp->next;
-            }
-            graph *add = new(graph);
-            add->kuda = b;
-            colors[b] = 'w';
-            add->next = NULL;
-            temp->next = add;
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-       if (colors[i] == 'w') search(i);
+        fin >> from >> where;
+        myGraph[from].push_back(where);
     }
 
-    if (crutch == -1) {
-        fout << "NO";
-    } else {
-        fout << "YES" << endl;
-        for (auto q: answer) {
-            fout << q << ' ';
-        }
+    for (int i = 0; i < n; i++) {
+        if (status[i] == 'w')
+            search(i);
     }
-
+    cout << "NO";
     return 0;
 }
