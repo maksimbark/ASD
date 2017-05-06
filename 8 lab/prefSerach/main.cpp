73639bdcdf6b2c338dd40d6ecadfc5868373a2c8
maksimbarkalov <maks@fizmatspb.ru>
2017-05-06 08:00:59 +0300

    prefSearch added

---
+++ 8 lab/prefSerach/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream fin("prefix.in");
+    ofstream fout("prefix.out");
+    string my;
+    int i = 1, j = 0;
+    fin >> my;
+    vector<int> p(my.length());
+
+    while (i < my.length()) {
+        if (my[i] == my[j]) {
+            p[i + 1] = j + 1;
+            ++j;
+            ++i;
+        } else {
+            if (j > 0)
+                j = p[j];
+            else {
+                p.push_back(0);
+                ++i;
+            }
+
+        }
+    }
+
+    for (int i = 1; i <= my.length(); ++i) {
+        fout << p[i] << " ";
+    }
+
+    return 0;
+}
